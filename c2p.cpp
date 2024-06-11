de63cc0e12325168a021535798d4d615b9b4b376
lunarvi <antoni.s.zieba@gmail.com>
2024-06-11 19:39:27 +0200

    C2P entry clean up

--- c2p.cpp
+++ c2p.cpp
@@ -6,19 +6,7 @@
 
 int main() {
     auto start = std::chrono::high_resolution_clock::now();
-//    for(int i = 0; i < 10; i++) {
-//        //std::string json = cfile2json("E:/Programming stuff/skanderbeg-api/providers/test.txt");
-//                std::string json = cfile2json("c:/Users/chiny/Documents/Paradox Interactive/Europa Universalis IV/save games/Castile1458_02_02.eu4");
-//    }
-//    auto end = std::chrono::high_resolution_clock::now();
-//    auto duration = duration_cast<std::chrono::milliseconds>(end - start);
-//    std::cout << "Execution time: " << duration.count() << std::endl;
-    // std::string json = cfile2json("E:/Programming stuff/skanderbeg-api/providers/test.txt");
-    std::string json = cfile2json(R"(c:\Users\chiny\Documents\Paradox Interactive\Victoria 3\save games\japanese shogunate_1836_01_01\gamestate)");
-     //   std::string json = cfile2json(R"(c:\Users\chiny\Downloads\Meshchyora1821_01_03\gamestate)");
-
-
-    //std::string json = cfile2json("c:/Users/chiny/Documents/Paradox Interactive/Victoria 3/save games/japanese shogunate_1836_01_01/gamestate");
+    std::string json = cfile2json(R"(test/input.eu4)");
 
     std::ofstream o("test.json");
     o << json;
@@ -27,7 +15,5 @@ int main() {
     auto end = std::chrono::high_resolution_clock::now();
     auto duration2 = duration_cast<std::chrono::microseconds>(end - start);
     std::cout << "Entire program run: " << duration2.count() << std::endl;
-
-
 
 }
